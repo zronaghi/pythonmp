9d3cc40b17d6e724044bc7bf814c87747f152fdb
Zahra Ronaghi <zronaghi@gmail.com>
2017-03-10 10:31:03 -0800

    Global v1, v2

--- numpy-dot-mp.py
+++ numpy-dot-mp.py
@@ -1,34 +1,47 @@
 import random
 import time
+import sys
 import multiprocessing
 from multiprocessing import Pool
 import numpy
 
-def dot_product_mp(chunk):
-    return numpy.dot (chunk[0], chunk[1])
+def dot_product_mp_new(chunk):
+    global v1, v2, step
+    start = time.time()
+    product = numpy.dot(v1[chunk:chunk+step], v2[chunk:chunk+step])
+    end = time.time()
+    #print 'inner time is', end-start
+    return product
 
 lower = -1.0
 upper = 1.0
-size = 10000000
-np = multiprocessing.cpu_count()
+size = int(sys.argv[1])
+np = int(sys.argv[2])
+print 'Array size is', size
+print '# of procs is', np
 step = size/np
 
 print "parallel numpy dot product on numpy arrays", np
+#v1 = numpy.random.uniform(lower, upper, size)
+#v2 = numpy.random.uniform(lower, upper, size)
 
-for j in range(10):
+#start = time.time()
+#result = numpy.dot(v1, v2)
+#print "Warm up", time.time() - start
+
+for j in range(4):
     v1 = numpy.random.uniform(lower, upper, size)
     v2 = numpy.random.uniform(lower, upper, size)
 
     chunks = list()
 
     for i in range(0, size, step):
-       chunks.append([v1[i:i+step], v2[i:i+step]])
-    
+        chunks.append(i)
     
     pool = Pool(processes=np)
 
     start = time.time()    
-    count = sum(pool.map(dot_product_mp, chunks) )
+    count = sum(pool.map(dot_product_mp_new, chunks) )
+    end = time.time()
    
-   
-    print j, time.time() - start, count
+    print j, end - start
